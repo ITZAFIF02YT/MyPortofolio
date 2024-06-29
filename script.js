56363cc1c7d763f80f137f5d6b5757878672a7ad
ITZAFIF02YT <mhdafiframadhany@gmail.com>
2024-06-29 15:13:27 +0700

    memberikan fungsi tambahan pada form

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contactForm');
-  
+    const status = document.getElementById('status')
     form.addEventListener('submit', function(event) {
       event.preventDefault();
-  
       const name = document.getElementById('name').value;
       const phone = document.getElementById('phone').value;
       const message = document.getElementById('message').value;
@@ -11,6 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
       // Nomor WhatsApp tujuan
       const waNumber = '6282392891122'; // format internasional tanpa tanda +
       
+      //validasi form
+    if (name === '' || phone === '' || message === '') {
+      status.textContent = 'Tolong isi formnya terlebih dahulu';
+      return;
+    } else {
+      status.textContent = ''; //reset pesan status jika form valid
+    }
       // Format pesan
       const waMessage = `Halo, nama saya ${name}. Nomor telepon saya adalah ${phone}. Pesan: ${message}`;
   
